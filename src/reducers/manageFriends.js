4f108e11176249030cbc92676d235a4faed8dd3d
emilycroft <emilyhcroft@gmail.com>
2017-02-06 23:06:44 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  if (action.type === "ADD_FRIEND") {
+    return Object.assign({}, state, {friends: state.friends.concat([action.payload])})
+  } else if (action.type === "REMOVE_FRIEND") {
+    let i = state.friends.findIndex(friend => friend.id === action.payload)
+    let newFriends = state.friends.slice(0, i).concat(state.friends.slice(i + 1))
+    return Object.assign({}, state, {friends: newFriends})
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+  if (action.type === "INCREASE") {
+    return {numberOfPresents: state.numberOfPresents + 1}
+    // let new_state = {...state, }
+  } else if (action.type === "DECREASE") {
+    return state.numberOfPresent - 1
+  } else {
+    return state
+  }
+
 }
